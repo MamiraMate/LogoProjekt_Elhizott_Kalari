b12a79a79ddaae668555e7b61df75d68f1cfb982
Mamira Máté <mamira.mate.21f@szlgbp.hu>
2022-03-03 08:48:28 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+	public partial class Form1 : Form
+	{
+		/* Függvények */
+
+
+		/* Függvények vége */
+		void FELADAT()
+		{
+			/* Ezt indítja a START gomb! */
+			// Teleport(közép.X, közép.Y+150, észak);
+
+			/// lehetséges feladatok
+
+			/*
+			 * széle: Máté
+			 * tetején lévő madár: 
+			 * várnak a torony része (amelyik kilóg a téglalapból):
+			 * felül lévő virág a vár mellett:
+			 * vár ajtaja: 
+			 * középen lévő cik-cak:
+			 * középen lévő hal feje:
+			 * középen lévő hal teste:
+			 * alul lévő zöld-fehér kerek virág:
+			 * alul lévő zöld-fehér elnyújtott virág:
+			 * alul lévő fehér magas virág:
+			*/
+		}
+	}
+}
