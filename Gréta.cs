cf6be565dd5924f3610f6886bd9eb43711ce7e5a
Greta00 <szatmari.greta.21f@szlgbp.hu>
2022-03-03 14:39:14 +0100

    torony

--- Gréta.cs
+++ Gréta.cs
@@ -11,6 +11,74 @@ namespace LogoKaresz
 {
     partial class Form1
     {
+        void téglalap(double szélesség, double magasság, Color szín)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Előre(magasság);
+                Jobbra(90);
+                Előre(szélesség);
+                Jobbra(90);
+            }
+            using (new Rajzol(false))
+            {
+                Előre(magasság / 2);
+                Jobbra(90);
+                Előre(szélesség / 2);
+                Tölt(szín);
+                Hátra(szélesség / 2);
+                Balra(90);
+                Hátra(magasság / 2);
+            }
+        }
+        void torony(double méret, Color szín, Color tollszín)
+        {
+            Tollszín(tollszín);
+            téglalap(méret * 2 + méret / 3, méret, szín);
+            Előre(méret);
+            téglalap(méret / 2, méret / 2, szín);
+            Jobbra(90);
+            Előre(méret / 2);
+            Balra(90);
+            Előre(méret / 2);
+            Jobbra(90);
+            Ív(90, méret / 2);
+            using (new Rajzol(false))
+            {
+                Jobbra(90);
+                Előre(méret / 8);
+                Jobbra(90);
+                Előre(méret / 8);
+                Tölt(szín);
+                Hátra(méret / 8);
+                Balra(90);
+                Hátra(méret / 8);
+                Jobbra(90);
+            }
+            Jobbra(90);
+            Előre(méret / 3 + méret / 2);
+            Balra(90);
+            téglalap(méret / 2, méret / 2, szín);
+            Balra(90);
+            Előre(méret / 2);
+            Jobbra(90);
+            Ív(90, méret / 2);
+            using (new Rajzol(false))
+            {
+                Jobbra(90);
+                Előre(méret / 8);
+                Jobbra(90);
+                Előre(méret / 8);
+                Tölt(szín);
+                Hátra(méret / 8);
+                Balra(90);
+                Hátra(méret / 8);
+                Jobbra(90);
+            }
 
+
+        }
+
+        
     }
 }
